70197e3022296b1a7661e81695b072fab8497868
alxayeed <alxayeed@gmail.com>
2022-06-25 15:55:02 +0600

    tasks completed

---
+++ Assignment 3/shoppingui/lib/screens/add_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class AddScreen extends StatelessWidget {
+  const AddScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Add Screen"));
+  }
+}
