c6e5adb1c24131bf0c3261e676f15e23fa3e3388
alxayeed <alxayeed@gmail.com>
2022-06-28 20:42:00 +0600

    project initialized

--- test /Test 1/Q8.dart
+++
@@ -1,9 +0,0 @@
-/*
-Ternery operation can be described as short form of conditionally executing any operation or statement.
- */
-
-void main(List<String> args) {
-  bool isWoman = false;
-
-  isWoman ? print("This way") : print("That way");
-}
